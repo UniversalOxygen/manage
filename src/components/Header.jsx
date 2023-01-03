95f38840b0fae8172c6fdddec0b89006f6ccdc81
unknown <olagoketunde600@gmail.com>
2023-01-03 00:55:15 -0600

    moving

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,15 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <div id='menuCon'>
-        <img id='shareBtn' src={shareBtn} alt="share button" />
-        <img id='menuBtn' src={menuBtn} alt="menu button" />
-      </div>
-      <div id='profileSection'>
-        <img id='profile_img' src={Img} alt="Profile" />
-        <h1 id='twitter'>UniversalOxygen</h1>
-        <h2 id='slack'>OXYGEN</h2>
-      </div>
+        gi
     </div>
 
   )
